c6757eb307fad978815193961838a76a36340d2a
eclnz <edward.clarkie@gmail.com>
2026-06-17 23:22:47 +1200

    a few comments for readability

--- Core/Src/mic.c
+++ Core/Src/mic.c
@@ -26,11 +26,15 @@ void transmit_audio(Mic *m, UART_HandleTypeDef *huart){
     if (m->audio_read_ready != MIC_BUF_EMPTY && HAL_UART_GetState(huart) == HAL_UART_STATE_READY) {
         m->audio_read_ready = MIC_BUF_EMPTY;
         memcpy(m->process_buf, m->half_buf_pos, sizeof(m->process_buf));
+        /* [0-3] SYNC_START */
         memcpy(m->tx_frame, &(uint32_t){SYNC_START}, 4);
+        /* [4-7] frame_seq */
         memcpy(m->tx_frame + 4, &m->frame_seq, 4);
         m->frame_seq++;
+        /* [8-1031] PCM samples. Assuming buffer is 1024 long */
         int32_buff_to_16(m->process_buf, (int16_t *)(m->tx_frame + 8), AUDIO_BUF_HALF);
-        memcpy(m->tx_frame + 8 + AUDIO_BUF_HALF * 2, &(uint32_t){SYNC_END}, 4);
+        /* [1032-1035] SYNC_END */
+        memcpy(m->tx_frame + 1032, &(uint32_t){SYNC_END}, 4);
         HAL_UART_Transmit_DMA(huart, m->tx_frame, sizeof(m->tx_frame));
     }
 }
