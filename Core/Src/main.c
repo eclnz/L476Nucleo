06203f6e7daca1f695411aaa3436c4e358e62c70
eclnz <edward.clarkie@gmail.com>
2026-06-21 20:59:24 +1200

    fix: baud rate to 250K for debugging

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -311,7 +311,7 @@ static void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 1000000;
+  huart2.Init.BaudRate = 250000;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
